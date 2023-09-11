741d200068e3d3315cc31c6eae32765bbe9cd042
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2023-09-11 11:34:47 +0300

    Added hash set test.

---
+++ Test/HashSetTest.c
@@ -0,0 +1,17 @@
+//
+// Created by Olcay Taner YILDIZ on 11.09.2023.
+//
+
+#include <stdio.h>
+#include "../src/HashMap/HashSet.h"
+
+int main(){
+    char* words[] = {"ali", "ayşe", "çift", "çöğüş", "öç", "şişe", "Ali", "Çift",
+                      "Çöğüş", "Şişe", "Öç"};
+    Hash_set_ptr set = create_hash_set_of_string(words, 11);
+    for (int i = 0; i < 6; i++){
+        if (!hash_set_contains(set, words[i])){
+            printf("Error in word %s\n", words[i]);
+        }
+    }
+}
