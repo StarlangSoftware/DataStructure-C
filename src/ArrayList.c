a9b9e59b8bc2c331321923d2c821ecbf447a2561
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2023-12-03 14:20:39 +0300

    Updated create_array_list_of_double.

--- src/ArrayList.c
+++ src/ArrayList.c
@@ -394,7 +394,7 @@ void array_list_clear(Array_list_ptr list, void (*free_method)(void *)) {
 Array_list_ptr create_array_list_of_double(int size) {
     Array_list_ptr values = create_array_list();
     for (int i = 0; i < size; i++){
-        double *value = calloc(1, sizeof(double));
+        double *value = calloc_(1, sizeof(double), "create_array_list_of_double");
         array_list_add(values, value);
     }
     return values;
